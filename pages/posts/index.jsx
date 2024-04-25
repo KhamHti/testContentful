b14f2b87b95b46116a6d8ccd51c0c99b9dde5f6b
Khun Kyaw Kham Hti <khamhti@agga.io>
2024-04-25 23:10:49 +0630

    error

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { client } from "../../lib/contentful/client";
+import PostCard from "../../components/posts/PostCard";
 
-const Posts = () => {
-  return <div className="text-3xl font-bold text-blue-300">Posts</div>;
+const Posts = ({ author }) => {
+  return (
+    <section className="section">
+      <div className="container">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:gap-10">
+          {author.map((author, i) => (
+            <PostCard key={author.fields.slug} author={author} />
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export const getStaticProps = async () => {
+  const response = await client.getEntries({ content_type: "author" });
+
+  return {
+    props: {
+      posts: response.items,
+      revalidate: 60,
+    },
+  };
 };
 
 export default Posts;
